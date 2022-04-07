c91c415b74187e3f6bcf1941b714e4d5ccdd3569
REFSZIN <refwire@gmail.com>
2022-04-07 00:50:47 -0300

    1.3
    CONTAGEM DAS CARTAS FEITO

--- script.js
+++ script.js
@@ -1,46 +1,48 @@
-    //GLOBAIS
-    // CARTAS 0 A 13 // 14 ITENS
     cartas= ['unicornparrot','unicornparrot', 'tripletsparrot', 'tripletsparrot', 'revertitparrot', 'revertitparrot', 'metalparrot', 'metalparrot', 'fiestaparrot', 'fiestaparrot', 'explodyparrot', 'explodyparrot', 'bobrossparrot','bobrossparrot'];
     qntcards = 0;
     let card = document.querySelector("article.card");
-    let front = document.querySelector("article.card > .front");
-    let back = document.querySelector("article.card > .back");
-    let mesa = document.querySelector("main > section");
+    let front = document.querySelectorAll("article.card > .front");
+    let back = document.querySelectorAll("article.card > .back");
+    let mesa =  document.querySelector("main > section");
     let config = document.querySelector("main > div")
     let baralho = [];
-    //VERIFICAR QNT DE CARDS 
-    //EMBARALHAR CARDS
-    //SELECIONAR CARD AND .FLIP  // CASO SEJA A 1° VIRAR APOS 1 CLIK. /// CASO SEJA 2° IF 1° === 2° MANTEM FRONT NONE // CASO TODAS VIRADAS WIN
-function configs(inicards){
-    qntcards= Number(prompt("Quantas Cartas Deseja ? Em pares (4) a (14)!"))
+function configs(){
+    qntcards = Number(prompt("Quantas Cartas Deseja ? Em pares (4) a (14)!"));
     while(qntcards %2 !== 0 || qntcards < 4|| qntcards>14){
-        qntcards= Number(prompt("Quantas Cartas Deseja ? Em pares (4) a (14)!"))
+    qntcards = Number(prompt("Quantas Cartas Deseja ? Em pares (4) a (14)!"));
     }
     mesa.classList.add("selecionado");
-    config.classList.add("esconder")
-    console.log(qntcards)
-    return (qntcards);
+    config.classList.add("esconder");
+    baralhocartas();
+    return qntcards;
 }
-
 function baralhocartas(){
-    baralho = cartas.slice(0,qntcards)
-    return (baralho);
-    console.log(baralho)
+    baralho = cartas.slice(0,qntcards);
+    console.log(baralho);
+    baralhado();
+    return baralho;
 }
 function baralhado(){
-    return Math.random() - 0.5; 
-    console.log(baralhado)
+    const embaralhado = Math.random() - 0.5;
+    embaralhador();
+    return embaralhado;
 }
-function embaralhador(){
-    baralhobaralhado= baralho.sort(baralhado);
-    return baralhobaralhado;
+function embaralhador(embaralhado){
+    const baralhobaralhado = baralho.sort(embaralhado);
     console.log(baralhobaralhado)
+    console.log(embaralhado);
+    mesapronta(baralhobaralhado);
 }
 function mesapronta(baralhobaralhado){
-    for (y = 0; y < baralhobaralhado.length; y++){  
+    for (let y = 0; y < baralhobaralhado.length; y++){  
         mesa.innerHTML+=`
         <article class="card" onclick="selecionarCard(this)">
             <div class="front"><img src="./assets/front.png" alt="Front Card" title="CORINGA"></div>
             <div class="back esconder"><img src='assets/${baralhobaralhado[y]}.gif' alt="parrot" title="parrot"></div> 
         </article>`}
-}   
+}
+function selecionarCard (cardx){
+    document.getElementsByClassName("article.card > .front").classList.add("esconder");
+    document.getElementsByClassName("article.card > .back").classList.remove("esconder")
+    document.getElementsByClassName("article.card > .back").classList.add("selecionado");
+}
