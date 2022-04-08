c4b3a22e6080e079a317a519c14c81facfc8bac0
REFSZIN <refwire@gmail.com>
2022-04-08 17:45:58 -0300

    Final Commit
    that's all, folks.

--- script.js
+++ script.js
@@ -15,11 +15,14 @@ const cartas = [
     "bobrossparrot"
 ];
 let firstcard = 0;
+let cronometro;
 let cardSelecionado;
 let qntcards = 0;
 let jogadas = 0;
-let minutos = document.getElementById('minutos').innerText = "00";
-let segundos =document.getElementById('segundos').innerText = "00";
+let segundos = 0;
+let minutos = 0;
+let segundo = document.getElementById('segundo');
+let minuto = document.getElementById('minuto');
 let card = document.querySelectorAll("article.card");
 let front = document.querySelectorAll("article.card > .front");
 let back = document.querySelectorAll("article.card > .back");
@@ -28,6 +31,7 @@ let config = document.querySelector("main > div.configcards");
 let recards = document.querySelector("input.recards");
 let inicards = document.querySelector("input.inicards");
 let confqntcards = document.getElementsByClassName("card virar");
+let musica = document.querySelector("header audio");
 let baralho = [];
 let embaralhado = [];
 
@@ -37,6 +41,7 @@ async function sleep(ms) {
 }
 function comparador() {
     return Math.random() - 0.5;
+    
 }
 function configs() {
     qntcards = Number(prompt("Quantas Cartas Deseja ? Em pares (4) a (14)!"));
@@ -49,7 +54,9 @@ function configs() {
     embaralhado = baralho.sort(comparador);
     embaralhado.sort(comparador);
     mesapronta(embaralhado);
-    timer();
+    cron();
+    musica.play()
+    musica.currentTime = 30;
 }
 function mesapronta(baralhobaralhado) {
     for (let y = 0; y < embaralhado.length; y++) {
@@ -89,12 +96,22 @@ async function finalizar(){
         config.classList.remove("esconder");
         recards.classList.add("selecionado");
         inicards.classList.add("esconder");
+        clearInterval(cronometro);
         alert(`Você ganhou em ${jogadas} jogadas!`);
         }
     recards.addEventListener("click", function reconfig(){
         location.reload();
     })
     }
-function timer(){
-
+    function cron(){
+    function segundo(){
+        segundos++;
+        if(segundos==60){
+        minutos++;
+        segundos=0;
+        document.getElementById('minuto').innerHTML=minutos;
+        }
+        document.getElementById('segundo').innerHTML=segundos;
+    }
+    cronometro = setInterval(function(){ segundo() },1000)
 }
